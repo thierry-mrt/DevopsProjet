8b0acefbf248fa4aafcb7d5580b9536f7a3fcdc1
thierry-mrt <thierry.martinelli@etu.univ-grenoble-alpes.fr>
2023-04-05 11:22:41 +0200

    Added jacoco and modif test to run it

--- test/TestDataFrame.java
+++ test/TestDataFrame.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDataFrame {
 
     @Test
-    public void Test(){
+    public void testFirst(){
         assertTrue(true, "test");
     }
 
