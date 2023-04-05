3f8e8428259ee312f703e3b119f9a84aeda3f939
thierry-mrt <thierry.martinelli@etu.univ-grenoble-alpes.fr>
2023-04-05 10:18:40 +0200

    Initial commit

---
+++ test/TestDataFrame.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDataFrame {
+
+    @Test
+    public void Test(){
+        assertTrue(true, "test");
+    }
+
+}
