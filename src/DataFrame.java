93fdd6f61aee324b99233b37dbfe4391c8315d44
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-19 08:48:24 +0200

    created getters

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -2,6 +2,14 @@ public class DataFrame {
     private Object[][] data;
     private String[] columnNames;
 
+    public Object[][] getData() {
+        return data;
+    }
+
+    public String[] getColumnNames() {
+        return columnNames;
+    }
+
     public DataFrame(Object[][] data, String[] columnNames) {
         this.data = data;
         this.columnNames = columnNames;
