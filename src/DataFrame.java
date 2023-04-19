07f2c97ee32c62ca82c603a56e68b07a353e829a
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-19 08:42:04 +0200

    created first constructor and attributes

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -1,2 +1,10 @@
 public class DataFrame {
+    private Object[][] data;
+    private String[] columnNames;
+
+    public DataFrame(Object[][] data, String[] columnNames) {
+        this.data = data;
+        this.columnNames = columnNames;
+    }
+
 }
