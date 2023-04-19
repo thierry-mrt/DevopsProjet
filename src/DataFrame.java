63d46c7dd67a4b1b639a5c66a0f488eecfc0f36e
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-19 09:37:30 +0200

    inserted index

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -1,18 +1,25 @@
 public class DataFrame {
+
     private Object[][] data;
+    private Integer[] index;
     private String[] columnNames;
 
     public Object[][] getData() {
         return data;
+    }
+    public Integer[] getIndex() {
+        return index;
     }
 
     public String[] getColumnNames() {
         return columnNames;
     }
 
-    public DataFrame(Object[][] data, String[] columnNames) {
+
+    public DataFrame(Object[][] data, Integer[] index, String[] columnNames) {
         this.data = data;
         this.columnNames = columnNames;
+        this.index = index;
     }
 
 }
