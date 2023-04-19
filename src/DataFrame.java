ec8b03121452426b684415b9aba6e86fe4db9708
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-19 10:58:12 +0200

    updated attribute types

--- src/DataFrame.java
+++ src/DataFrame.java
@@ -1,22 +1,63 @@
+import java.util.ArrayList;
+
 public class DataFrame {
 
-    private Object[][] data;
-    private Integer[] index;
-    private String[] columnNames;
+    private ArrayList<ArrayList<Object>> data;
+    private ArrayList<Integer> index;
+    private ArrayList<String> columnNames;
 
-    public Object[][] getData() {
+    public ArrayList<ArrayList<Object>> getData() {
         return data;
     }
-    public Integer[] getIndex() {
+    public ArrayList<Integer> getIndex() {
         return index;
     }
 
-    public String[] getColumnNames() {
+    public ArrayList<String> getColumnNames() {
         return columnNames;
     }
 
 
-    public DataFrame(Object[][] data, Integer[] index, String[] columnNames) {
+    public DataFrame(ArrayList<ArrayList<Object>> data, ArrayList<Integer> index, ArrayList<String> columnNames) {
+
+        if (data.size() != columnNames.size()) {
+            throw new IllegalArgumentException("Le nombre de colonnes de données est différent du nombre de noms de colonnes");
+        }
+
+        int max = 0;
+        for (ArrayList<Object> colonne : data) {
+            if (colonne.size() > max) max = colonne.size();
+        }
+        if (max != index.size()) {
+            throw new IllegalArgumentException("Le nombre d'index est différent du nombre de lignes");
+        }
+
+        boolean contientDoublonsColonnes = false;
+        for (int i = 0; i < columnNames.size(); i++) {
+            for (int j = i + 1; j < columnNames.size(); j++) {
+                if (columnNames.get(i).equals(columnNames.get(j))) {
+                    contientDoublonsColonnes = true;
+                    break;
+                }
+            }
+        }
+        if (contientDoublonsColonnes) {
+            throw new IllegalArgumentException("Le nom des colonnes ne doit pas être identique");
+        }
+
+        boolean contientDoublonsIndex = false;
+        for (int i = 0; i < index.size(); i++) {
+            for (int j = i + 1; j < index.size(); j++) {
+                if (index.get(i).equals(index.get(j))) {
+                    contientDoublonsIndex = true;
+                    break;
+                }
+            }
+        }
+        if (contientDoublonsIndex) {
+            throw new IllegalArgumentException("Les index ne doivent pas être identiques");
+        }
+
         this.data = data;
         this.columnNames = columnNames;
         this.index = index;
