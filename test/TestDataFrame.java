06b72624753f8f575eb60260a483129d586185b6
sciascil <lucas.sciascia@gmail.com>
2023-04-25 14:28:28 +0200

    test modif

--- test/TestDataFrame.java
+++ test/TestDataFrame.java
@@ -11,7 +11,7 @@ public class TestDataFrame {
 
     @Test
     public void testdataframetest2(){
-        assertTrue(false, "test du 2eme test");
+        assertTrue(true, "test du 2eme test");
     }
 
 }
