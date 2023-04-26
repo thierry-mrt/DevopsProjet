5eb05735539b59ac826561278c7ee2118936dcfb
thierry-mrt <thierry.martinelli@etu.univ-grenoble-alpes.fr>
2023-04-26 15:56:05 +0200

    Modification nom fichier test constructeur 1

---
+++ test/TestDataFrameConst1.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDataFrameConst1 {
+
+    @Test
+    public void testFirst(){
+        assertTrue(true, "test");
+    }
+
+}
