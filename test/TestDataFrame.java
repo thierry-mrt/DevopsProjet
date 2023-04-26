1c13325e15a621e2704c73ea5ac65defe8b8e0c1
sciascil <lucas.sciascia@gmail.com>
2023-04-26 08:51:21 +0200

    test22 : report tests

--- test/TestDataFrame.java
+++ test/TestDataFrame.java
@@ -11,7 +11,7 @@ public class TestDataFrame {
 
     @Test
     public void testdataframetest2(){
-        assertTrue(true, "test du 2eme test");
+        assertTrue(false, "test du 2eme test");
     }
 
 }
