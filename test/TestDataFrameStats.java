5c1404fe0b538f8402711ca05eb315368e0caf71
Abdoulhakim Tangara <abdoulhakim.tangara@etu.univ-grenoble-alpes.fr>
2023-04-26 15:52:01 +0200

    création sequelette pour méthodes de test

---
+++ test/TestDataFrameStats.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Test;
+
+public class TestDataFrameStats {
+
+    private Object actual, expected;
+
+    public TestDataFrameStats() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMoyenneColonne() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMoyenneColonneVide() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMoyenneColonneInexistante() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMoyenneColonneNonNumerique() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMaximumColonne() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMaximumColonneVide() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMaximumColonneInexistante() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMaximumColonneNonNumerique() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMinimumColonne() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMinimumColonneVide() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMinimumColonneInexistante() {
+        // TODO
+    }
+
+    @Test
+    public void testCalculerMinimumColonneNonNumerique() {
+        // TODO
+    }
+
+    @Test
+    public void testColonneNonNumerique() {
+
+    }
+
+    @Test
+    public void testColonneInexistante() {
+        // TODO
+    }
+
+}
